da4abfd1e375bbe361999c0ee3a99f8ded51bfde
tzirkus <zalmen5779@gmail.com>
2025-11-03 11:03:09 +0200

    fix structur

--- examples/example_main.py
+++ examples/example_main.py
@@ -1,6 +1,5 @@
 from menu_building.menu_builder import MenuBuilder
 from io_selection.console_io import ConsoleIo
-from menu_building.main_manu import MainMenu
 
 
 
@@ -24,19 +23,20 @@ def print_name(*names):
 
 
 # menu = {'print': {'hello': {'world': print_world, 'home': print_home}, 'world': print_world}, 'print name': print_name}
-hello = MainMenu('hello')
+
+io = ConsoleIo()
+
+hello = MenuBuilder(io=io, title= 'hello')
 hello.add_function_item(name = 'home', function=print_home)
 hello.add_function_item(name = 'world', function=print_world)
 
-printt = MainMenu('print')
+printt = MenuBuilder(io=io, title='print')
 printt.add_sub_menu_item('hello', sub_menu= hello)
 printt.add_function_item(name = 'world', function=print_world)
 
-main_menu = MainMenu('main menu')
+main_menu = MenuBuilder(io = io, title = 'main menu')
 main_menu.add_sub_menu_item('print', sub_menu=printt)
 main_menu.add_function_item('zalmen', 'shneyor', name = 'print name', function=print_name)
 
-io = ConsoleIo()
-t = MenuBuilder(io)
-t.build_menu(menu=main_menu, requested_main='10', requested_back='20', requested_exit='30')
-t.build_menu(main_menu)
+main_menu.run_menu(menu=main_menu, requested_exit='30', requested_main='10', requested_back='20')
+# main_menu.build_menu(main_menu)
--- menu_building/menu_builder.py
+++ menu_building/menu_builder.py
@@ -1,14 +1,23 @@
-from typing import Union, List
-from menu_building.main_manu import MainMenu
+from typing import Union, List, Callable
 from menu_building.menu_item import MenuItem
 
 
 class MenuBuilder:
 
-    def __init__(self, io) -> None:
+    def __init__(self, io, title: str) -> None:
         self.io = io
+        self.title = title
+        self.items: List[MenuItem] = []
 
-    def build_menu(self, menu: MainMenu, requested_exit: str = '*',requested_main:str = '#', requested_back: str = '0', is_root: bool = True) -> Union[str, None]:
+    def add_sub_menu_item(self, name: str, sub_menu: 'MenuBuilder'):
+        item = MenuItem(name=name, sub_menu=sub_menu)
+        self.items.append(item)
+
+    def add_function_item(self, *args, name: str, function: Callable):
+        item = MenuItem(name=name, function=function, args=args)
+        self.items.append(item)
+
+    def run_menu(self, menu: 'MenuBuilder', requested_exit: str = '*',requested_main:str = '#', requested_back: str = '0', is_root: bool = True) -> Union[str, None]:
         while True:
             for number, item in enumerate(menu.items):
                 self.io.output(f'{number + 1} - {item.name}')
@@ -35,10 +44,14 @@ class MenuBuilder:
                 self.io.output('invalid choice!! please enter your choice again')
                 continue
             choose_option: MenuItem = items[index_choose]
+
             if choose_option.is_function():
                 choose_option.execute()
+
             elif choose_option.is_sub_menu():
-                result = self.build_menu(choose_option.sub_menu, requested_exit, requested_main, requested_back, is_root = False)
+                # result = self.run_menu(choose_option.sub_menu, requested_exit, requested_main, requested_back,
+                #                        is_root=False)
+                result = choose_option.sub_menu.run_menu(choose_option.sub_menu, requested_exit, requested_main, requested_back, is_root=False)
                 if result == 'exit':
                     return 'exit'
                 if result == 'main':
